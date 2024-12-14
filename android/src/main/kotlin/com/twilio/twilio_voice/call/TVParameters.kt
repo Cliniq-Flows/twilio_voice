e1b25cac913798c95a23523293a20826d85f2d15
Erchil66 <92772509+Erchil66@users.noreply.github.com>
2024-12-15 01:57:45 +0800

    changes

--- android/src/main/kotlin/com/twilio/twilio_voice/call/TVParameters.kt
+++ android/src/main/kotlin/com/twilio/twilio_voice/call/TVParameters.kt
@@ -24,7 +24,7 @@ interface TVParameters {
 
     companion object {
         val PARAM_CALLER_ID: String = "__TWI_CALLER_ID"
-        val PARAM_CALLER_NAME: String = "__TWI_CALLER_NAME"
+        val PARAM_CALLER_NAME: String = "firstname"
 
         val PARAM_RECIPIENT_ID: String = "__TWI_RECIPIENT_ID"
         val PARAM_RECIPIENT_NAME: String = "__TWI_RECIPIENT_NAME"
