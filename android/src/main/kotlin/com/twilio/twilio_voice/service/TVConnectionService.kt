70594ae06ac32679c12a977df25587422268de3d
Erchil66 <92772509+Erchil66@users.noreply.github.com>
2025-06-12 14:55:07 +0800

    ss

--- android/src/main/kotlin/com/twilio/twilio_voice/service/TVConnectionService.kt
+++ android/src/main/kotlin/com/twilio/twilio_voice/service/TVConnectionService.kt
@@ -586,7 +586,7 @@ class TVConnectionService : ConnectionService() {
                     // 5) (Optional) keep a reference if you use TVCallConnection for other things
                     // 3) Create & configure your TVCallConnection
 
-                    twilioCall.sid?.let { sid ->
+                twilioCall.sid?.let { sid ->
                     activeConnections[sid] = conn
                     attachCallEventListeners(conn, sid)
                     }
