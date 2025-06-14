24959d919bf5e27e9e24948fff30c383c95985d9
Erchil66 <92772509+Erchil66@users.noreply.github.com>
2025-06-14 22:16:27 +0800

    test x

--- android/src/main/kotlin/com/twilio/twilio_voice/service/TVConnectionService.kt
+++ android/src/main/kotlin/com/twilio/twilio_voice/service/TVConnectionService.kt
@@ -11,6 +11,10 @@ import android.content.Context
 import android.content.Intent
 import com.twilio.voice.CallException
 import android.content.pm.ServiceInfo
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.Ringtone
+import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -350,8 +354,14 @@ class TVConnectionService : ConnectionService() {
                          pendingInvite = callInvite
                           val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
                         ringtone = RingtoneManager.getRingtone(applicationContext, uri)?.apply {
-                            streamType = AudioManager.STREAM_RING
-                            isLooping = true
+//                            streamType = AudioManager.STREAM_RING
+                            audioAttributes = AudioAttributes.Builder()
+                                .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+                                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                                .build()
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                                isLooping = true
+                            }
                             play()
                         }
                         // app is open → skip system UI, just notify your plugin
