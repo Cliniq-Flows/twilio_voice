3a22b2abe0c2e43f07dee150b46c45d9f7c18eb0
Erchil66 <92772509+Erchil66@users.noreply.github.com>
2025-06-17 13:52:14 +0800

    test

--- android/src/main/kotlin/com/twilio/twilio_voice/service/TVConnectionService.kt
+++ android/src/main/kotlin/com/twilio/twilio_voice/service/TVConnectionService.kt
@@ -353,7 +353,7 @@ class TVConnectionService : ConnectionService() {
                     // Add new incoming call to the telecom manager
                   //  telecomManager.addNewIncomingCall(phoneAccountHandle, extras)
                     if (AppState.isFlutterForeground) {
-                        Log.e(TAG, "APP IS RESUMED AND GOES HERE TO RING RING")
+                        Log.e(TAG, "APP IS RESUMED AND GOES HERE TO RING RING MTF")
 
                         val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
                         ringtone = RingtoneManager.getRingtone(applicationContext, uri)?.apply {
